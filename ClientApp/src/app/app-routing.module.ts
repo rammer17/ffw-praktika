7e78fe5bb342045cfc99ff55de0410822ad66e0d
Georgi <georgi.rosenov17@gmail.com>
2022-07-05 12:17:25 +0300

    fixed issue with bootstrap

--- ClientApp/src/app/app-routing.module.ts
+++ ClientApp/src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DinoPreviewComponent } from './dino/dino-preview/dino-preview.component';
+import { HomeComponent } from './home/home.component';
+import { ShoppingCartComponent } from './shopping-cart/shopping-cart.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: 'home', component:  HomeComponent },
+  { path: 'dino-preview', component:  DinoPreviewComponent },
+  { path: 'cart', component:  ShoppingCartComponent },
+  { path: '**', redirectTo: 'home', pathMatch: 'full' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- ClientApp/src/app/app.module.ts
+++ ClientApp/src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { DinoPreviewComponent } from './dino/dino-preview/dino-preview.component';
 import { HomeComponent } from './home/home.component';
+import { ShoppingCartComponent } from './shopping-cart/shopping-cart.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     DinoPreviewComponent,
-    HomeComponent
+    HomeComponent,
+    ShoppingCartComponent
   ],
   imports: [
     BrowserModule,
