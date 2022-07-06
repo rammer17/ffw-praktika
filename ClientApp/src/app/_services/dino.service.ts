fa9b9873c99497cceb510a6bc58754cfa93eff01
Georgi <georgi.rosenov17@gmail.com>
2022-07-06 09:37:41 +0300

    smh

---
+++ ClientApp/src/app/_services/dino.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from "@angular/core";
+import { IDino } from "../_models/dino.model";
+
+@Injectable({
+    providedIn: 'root'
+})
+export class DinoService {
+    DinoList: IDino[] = [
+        {
+            id: 1,
+            title: 'Бронтозавър',
+            description: 'Описание',
+            price: 1200
+        }
+    ];
+}
